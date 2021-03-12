10ff3eeb433ec28c2ad7411e30f0a22bcf5f2a86
felipeschneider88 <felipes88@gmail.com>
2021-03-12 11:22:40 -0300

    create base project and add dependences

---
+++ ci-database/Todo.Deploy/Program.cs
@@ -0,0 +1,52 @@
+﻿using DbUp;
+using Npgsql;
+using System;
+
+namespace Todo.Deploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting the CI to deploy DB changes");
+            // This will load the content of .env file and create related environment variables
+            DotNetEnv.Env.Load();
+
+            // Connection string for deploying the database (high-privileged account as it needs to be able to CREATE/ALTER/DROP)
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+            // Password for the user that will be used by the API to connect to Azure SQL (low-privileged account)
+            var backEndUserPassword = Environment.GetEnvironmentVariable("BackEndUserPassword");
+
+            var csb = new  NpgsqlConnectionStringBuilder(connectionString);
+            Console.WriteLine($"Deploying database: {csb.Database}");
+
+            Console.WriteLine("Testing connection...");
+            var conn = new NpgsqlConnection(csb.ToString());
+            conn.Open();
+            conn.Close();
+
+            Console.WriteLine("Starting deployment...");
+            var dbup = DeployChanges.To
+                .SqlDatabase(csb.ConnectionString)
+                .WithScriptsFromFileSystem("./sql")
+                .JournalToSqlTable("dbo", "$__dbup_journal")
+                .WithVariable("BackEndUserPassword", backEndUserPassword)
+                .LogToConsole()
+                .Build();
+
+
+            var result = dbup.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.WriteLine(result.Error);
+                //Add own 5000 error code to exit
+                Environment.Exit(5000);
+            }
+
+            Console.WriteLine("Success!");
+            Environment.Exit(-1);
+        }
+    }
+}
