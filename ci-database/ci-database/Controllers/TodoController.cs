2f08933d51373fe12a202362b14ba16b06cb33e6
felipeschneider88 <felipes88@gmail.com>
2021-03-12 16:00:10 -0300

    query all data from table todos

--- ci-database/ci-database/Controllers/TodoController.cs
+++ ci-database/ci-database/Controllers/TodoController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Npgsql;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,9 +27,22 @@ namespace ci_database.Controllers
             _config = config;
         }
         [HttpGet]
-        public IEnumerable<string> get()
+        public async Task<JToken> get()
         {
-            return new string[] { "value1", "value2" };
+            using (var conn = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+            {
+                string query = " select * from todos";
+                conn.Open();
+                NpgsqlCommand selectCommand = new NpgsqlCommand(query, conn);
+                NpgsqlDataReader reader = selectCommand.ExecuteReader(); 
+                var datatable = new DataTable();
+                datatable.Load(reader);
+                string JsonResponse = string.Empty;
+                JsonResponse = JsonConvert.SerializeObject(datatable);
+                reader.Close();
+                conn.Close();
+                return JToken.Parse(JsonResponse);
+            }
         }
 
         // GET api/<TodoController>/5
@@ -52,5 +69,28 @@ namespace ci_database.Controllers
         public void Delete(int id)
         {
         }
+
+        //private JToken EnrichResult(string source)
+        //{
+        //    var jr = JToken.Parse(source ?? "[]");
+
+        //    var baseUrl = (HttpContext != null) ? HttpContext.Request.Scheme + "://" + HttpContext.Request.Host : string.Empty;
+
+        //    var AddUrl = new Action<JObject>(o =>
+        //    {
+        //        if (o == null) return;
+        //        var todoUrl = $"{baseUrl}/todo/{o["id"]}";
+        //        o["url"] = todoUrl;
+        //    });
+
+        //    if (jr is JArray ja)
+        //        ja.ToList().ForEach(e => AddUrl(e as JObject));
+        //    else if (jr is JObject jo)
+        //        AddUrl(jo);
+        //    else
+        //        throw new ArgumentException($"{nameof(source)} is not an array or an object");
+
+        //    return jr;
+        //}
     }
 }
--- ci-database/ci-database/Startup.cs
+++ ci-database/ci-database/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ci_database
 {
@@ -28,6 +29,7 @@ namespace ci_database
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen();
             services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
 
         }
 
